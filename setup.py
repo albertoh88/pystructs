2dd38bb014beb83b4a04c6a1ca1c2c2d40185805
albertoh88 <albertoh8806@gmail.com>
2025-08-29 12:48:28 -0300

    chore: add setup.py for packaging and distribution

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pystructs",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="Functional utilities for nested data structures and validations",
+    author="albertoh88",
+    license="MIT",
+)
