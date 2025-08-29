76ee1aa2dde7cab2bfeedc2d5322cb5c387479e9
albertoh88 <albertoh8806@gmail.com>
2025-08-29 14:11:40 -0300

    chore: update setup.py with package metadata and Python version requirement

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     packages=find_packages(),
     install_requires=[],
     description="Functional utilities for nested data structures and validations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="albertoh88",
     license="MIT",
+    python_requires=">=3.8,<3.13"
 )
